00c564a4dff5f8678bf0c2f9234320042f3bfc71
Andre Lucyo <andre.lucyo2@gmail.com>
2021-02-04 23:12:20 -0300

    criado metodos pacar carrgar objeto a artir de um json e converter objeto em json

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,4 +1,20 @@
 class Item {
   String title; //titulo
   bool done; //se esta feito
+
+  Item({this.title, this.done});
+
+  //converte um json: video 5 https://youtu.be/1Ke4cB8K4Q4?list=PLHlHvK2lnJndhgbqLl5DNEvKQg5F4ZenQ&t=249
+
+  Item.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    done = json['done'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['title'] = this.title;
+    data['done'] = this.done;
+    return data;
+  }
 }
