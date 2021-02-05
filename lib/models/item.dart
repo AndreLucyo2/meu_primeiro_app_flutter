d3d6e75031bffd7eb98401a9833d1210fdc7ed1c
Andre Lucyo <andre.lucyo2@gmail.com>
2021-02-04 23:15:30 -0300

    Gerador de json to dart: https://javiercbk.github.io/json_to_dart/

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -6,12 +6,15 @@ class Item {
 
   //converte um json: video 5 https://youtu.be/1Ke4cB8K4Q4?list=PLHlHvK2lnJndhgbqLl5DNEvKQg5F4ZenQ&t=249
 
+  //Gerador de json to dart: https://javiercbk.github.io/json_to_dart/
+
   Item.fromJson(Map<String, dynamic> json) {
     title = json['title'];
     done = json['done'];
   }
 
   Map<String, dynamic> toJson() {
+    //constante:
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['title'] = this.title;
     data['done'] = this.done;
